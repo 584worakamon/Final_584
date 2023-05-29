02df35b1d5e062650f4c8a1e697515cf0a6302a9
584worakamon <124670752+584worakamon@users.noreply.github.com>
2023-05-29 11:58:45 +0700

    Add files via upload

---
+++ restaurantpoint.js
@@ -0,0 +1,158 @@
+var restaurantpoint =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "SavoeyThaMaharaj",
+        "ประเภท": "อาหารซีฟู้ด",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.48871173538669,
+          13.755338198008218
+        ],
+        "type": "Point"
+      },
+      "id": 0
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "LukKaiThong",
+        "ประเภท": "อาหารไทย-จีน",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.51120366741367,
+          13.726703552194238
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "OHKAJHU",
+        "ประเภท": "อาหารเฮลตี้",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.53237194723113,
+          13.744854061191162
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "BanNamKiengDin",
+        "ประเภท": "อาหารผสมผสาน",
+        "คะแนนความนิยม": 5
+      },
+      "geometry": {
+        "coordinates": [
+          100.35783338758671,
+          13.777212548193347
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "Ranee'srestaurant",
+        "ประเภท": "อาหารอิตาเลี่ยน",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.49575866741145,
+          13.758824724515492
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "SanaeChan",
+        "ประเภท": "อาหารไทย",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.55154672343645,
+          13.839422316330044
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "KonchongKonprung",
+        "ประเภท": "อาหารไทย-จีน",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.55291600325972,
+          13.788970369708593
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "Somsakpuob",
+        "ประเภท": "อาหารซีฟู้ด",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.49637100325828,
+          13.727012552170933
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "OngtongKhaosoi",
+        "ประเภท": "อาหารเหนือ",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.54458972343662,
+          13.781144724732584
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ร้านอาหาร": "BaanBenjarongPai",
+        "ประเภท": "อาหารไทย",
+        "คะแนนความนิยม": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.4483812830847,
+          13.809587545617617
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
